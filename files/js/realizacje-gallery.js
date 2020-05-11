e8eae396b75a896643b562f6f721ec01251c4847
jackosky <jckporeda@gmail.com>
2020-05-11 10:30:03 +0200

    max width 1600

--- files/js/realizacje-gallery.js
+++ files/js/realizacje-gallery.js
@@ -24,18 +24,22 @@ function getImages(name, count) {
     return imgs;
 }
 
-$('#galler-link-1').click(function () {
+$('#galler-link-1').click(function (e) {
+    e.preventDefault();
     prepareGallery('suspenzo', 9);
 });
 
-$('#galler-link-2').click(function () {
+$('#galler-link-2').click(function (e) {
+    e.preventDefault();
     prepareGallery('tim_grey', 9);
 });
 
-$('#galler-link-3').click(function () {
+$('#galler-link-3').click(function (e) {
+    e.preventDefault();
     prepareGallery('wegielek', 9);
 });
 
-$('#galler-link-4').click(function () {
+$('#galler-link-4').click(function (e) {
+    e.preventDefault();
     prepareGallery('inne', 3);
 });
