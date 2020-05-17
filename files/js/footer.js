b2fbc0a4855efb16014ee49e3aea8bfa07ad2786
jackosky <jckporeda@gmail.com>
2020-05-17 20:26:03 +0200

    final verison

--- files/js/footer.js
+++ files/js/footer.js
@@ -1,6 +1,25 @@
+//menu handling
+$('.nav-link').click(function () {
+    $('.nav-link img').remove();
+    $(this).append('<img class="d-none d-lg-block menu-link" width="20px" src="./files/img/icons/trojkat_stopka_gora.svg">');
+});
+
+
+positionPhoneContact();
+$(window).resize(function () {
+    positionPhoneContact();
+});
+
+function positionPhoneContact() {
+    if ($(window).width() > 1600) {
+        $('.sticky-contact').css('right', ($(window).width() - 1600) / 2 + 'px');
+    }
+    $('.sticky-contact').addClass('d-md-block');
+}
+
 //footer
 let maxScroll = $(document).height() - $(window).height();
-$(window).scroll(function(event){
+$(window).scroll(function (event) {
     let currentScroll = $(window).scrollTop();
     let scrollToHidFooter = maxScroll * 0.8;
     let scrollToOpenFooter = maxScroll * 0.95;
@@ -11,4 +30,6 @@ $(window).scroll(function(event){
     if (currentScroll > scrollToHidFooter && currentScroll < scrollToOpenFooter) {
         $('#collapsableFooter').collapse('hide')
     }
- });
+});
+
+
