02c4825ac82dcb2497daa83d9cc18adfe83e2c27
jackosky <jckporeda@gmail.com>
2020-05-24 12:55:05 +0200

    fix moving gallery indicator

--- files/js/common.js
+++ files/js/common.js
@@ -9,6 +9,7 @@ $('a[href*="#"]')
   // Remove links that don't actually link to anything
   .not('[href="#"]')
   .not('[href="#sticky-contact-txt"]')
+  .not('[href="#carouselIndicators"]')
   .not('[href="#0"]')
   .click(function(event) {
     // On-page links
