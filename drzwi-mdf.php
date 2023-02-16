b519d521aecd3d34e0208980ffbe0c92f65332f2
SeoBS2023 <123944535+SeoBS2023@users.noreply.github.com>
2023-02-16 12:24:30 +0100

    Add files via upload

---
+++ drzwi-mdf.php
@@ -0,0 +1,207 @@
+<!DOCTYPE html>
+<!-- saved from url=(0014)about:internet -->
+<html lang="pl-PL">
+
+<head>
+  <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+  <link rel="icon" href="./files/img/icons/MultiDoor_logo.svg">
+
+  <title>Maxidoor</title>
+
+  <link rel="canonical" href="">
+
+  <!-- Bootstrap core CSS -->
+  <link href="./files/external/bootstrap.min.css" rel="stylesheet">
+
+  <!-- Custom styles for this template -->
+  <link href="./files/css/main.css" rel="stylesheet">
+  <link href="./files/css/oferta.css" rel="stylesheet">
+  <link href="./files/css/header-carousel.css" rel="stylesheet">
+  <link href="./files/css/realizacje-carousel.css" rel="stylesheet">
+  <link href="./files/css/table.css" rel="stylesheet">
+</head>
+
+<body>
+  <nav class="site-header sticky-top navbar navbar-expand-lg py-1 pb-0">
+    <a class="navbar-brand site-header-ml" href="#">
+      <span class="d-inline-block align-top">
+        <img class="logo img-fluid" src="./files/img/icons/logo.svg" alt="maxidoor logo" />
+      </span>
+    </a>
+    <!-- mobile toggler -->
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse"
+      data-target="#navbarSupportedContent" aria-controls="navbarNavDropdown" aria-expanded="false"
+      aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+      <ul class="navbar-nav ml-auto">
+        <li class="nav-item mt-2 ml-3 mr-3 mb-0 pb-0">
+          <a class="nav-link mb-0 pb-0" href="index.html">Strona Główna
+          </a>
+
+        </li>
+        <li class="nav-item m-2 ml-3 mr-3 mb-0 pb-0">
+          <a class="nav-link mb-0 pb-0" href="index.html#onas-content">O nas
+          </a>
+
+        </li>
+        <li class="nav-item m-2 ml-3 mr-3 mb-0 pb-0">
+          <a class="nav-link mb-0 pb-0" href="oferta.html">Oferta
+            <img class="d-none d-lg-block menu-link" width="20px" src="./files/img/icons/trojkat_stopka_gora.svg">
+          </a>
+
+        </li>
+        <li class="nav-item m-2 ml-3 mr-3 mb-0 pb-0">
+          <a class="nav-link mb-0 pb-0" href="index.html#realizacje-content">Realizacje
+          </a>
+        </li>
+        <li class="nav-item m-2 ml-3 mr-3 mb-0 pb-0">
+          <a class="nav-link mb-0 pb-0" href="index.html#kontakt-content">Kontakt
+          </a>
+        </li>
+      </ul>
+    </div>
+  </nav>
+
+  <div id="sticky-contact" class="sticky-contact d-none">
+    <div id="sticky-contact-btn">
+      <a data-toggle="collapse" href="#sticky-contact-txt" role="button" aria-expanded="false"
+        aria-controls="sticky-contact-txt">
+        <img src="./files/img/icons/telefon_po_prawej_ekranu.svg">
+      </a>
+    </div>
+    <div id="sticky-contact-txt" class="p-4 collapse">
+      <p class="font-weight-bold text-uppercase">Kontakt</p>
+      <small>
+        <p>
+          601 530 542<br>
+          601 230 532<br>
+          info@maxidoor.pl
+        </p>
+      </small>
+    </div>
+  </div>
+
+ 
+
+
+  <div id="mdf-content" class="container-fluid section-height content-separator background-color-white overflow-hidden">
+    <div class="row">
+      <div class="col-lg-5 pl-5 pr-5 my-auto pt-5 pb-5">
+        <div class="row ml-3">
+          <h2 class="font-weight-bold color-red">Drzwi MDF</h2>
+          <p class="mt-5">Materiał, z jakiego wykonane są drzwi wpływa nie tylko na ich wygląd, ale i na cenę. Do jednych z najpopularniejszych należą drzwi MDF, czyli wykonane z płyty MDF (Medium Density Fibreboard), nieco mocniejszej wersji płyty wiórowej. To największy konkurent drzwi drewnianych, choć podobnie jak drzwi wykonane z naturalnego surowca są one doskonałym sposobem na zachowanie w domu komfortu i poczucia bezpieczeństwa.
+          </p>
+          <p class="mt-5">Płyta MDF, z której wykonane są drzwi posiada większą gęstość, wytwarzana jest również w wyższej temperaturze, co wpływa na jej właściwości. Jest odporna na uszkodzenia, ma bardzo wysoką izolację akustyczną.</p>
+          <h1 class="font-weight-bold color-red">Drzwi MDF – Warszawa</h1>
+          <p class="mt-5">Drzwi MDF pokryte są powłoką – fornirem lub lakierem, co zapewnia im właściwy i elegancki wygląd. Fornir to cienkie płaty drewna, którymi pokrywa się meble oraz drzwi, zaś lakier to roztwór, który może nadać drzwiom dowolny kolor. Drzwi MDF najczęściej pokrywane są lakierem, który można swobodnie dopasować do panujących trendów, ale i do kolorystyki wnętrz, do jakich dopasowywane są drzwi.
+          </p>
+          <p class="mt-5">Ponieważ płyta MDF charakteryzuje się wysoką gęstością, drzwi MDF są trwałe i odporne na uszkodzenia, co sprawia że są chętnie wybierane przez inwestorów. Dodatkowo posiadają doskonałe właściwości akustyczne, dzięki czemu chętnie wybierane są jako drzwi wewnętrze do sypialni, czy pokoju dziecka zwiększając komfort snu i wypoczynku.</p>
+          <h2 class="font-weight-bold color-red">Wyjątkowa odporność drzwi MDF</h2>
+          <p>Odpowiednie wytworzenie płyt MDF sprawia, że drzwi MDF są również stosunkowo odporne na wilgoć, dlatego też z powodzeniem można je zastosować jako drzwi od łazienki, z zastosowaniem odpowiednich otworów wentylacyjnych.</p>
+          <p>Drzwi MDF to doskonała alternatywa dla klasycznych drzwi drewnianych – choć są nieco cięższe, posiadają podobne, a niekiedy nawet lepsze właściwości akustyczne i izolacyjne, co zwiększa ich konkurencyjność. Czy są lepsze niż drewniane? To zależy już od indywidualnych preferencji inwestorów – my możemy jedynie zapewnić, że jedne i drugie wykonujemy zgodnie z projektem.</p>
+        </div>
+      </div>
+
+      <div class="col-lg-7 p-0">
+        <img class="img-fit" src="./files/img/oferta/MDF.png">
+      </div>
+    </div>
+  </div>
+
+
+  <footer class="container-fluid sticky-bottom d-none d-lg-block">
+    <a class="btn btn-arrow collapsed" type="button" data-toggle="collapse" aria-pressed="false"
+      data-target="#collapsableFooter" aria-expanded="false" aria-controls="collapsableFooter">
+      <img class="footer-button-up" width="20px" src="./files/img/icons/trojkat_stopka_gora.svg">
+      <img class="footer-button-down" width="20px" src="./files/img/icons/trojkat_stopka_dol.svg">
+    </a>
+    <div id="collapsableFooter" class="collapse pt-5">
+      <div class="row color-white">
+        <div class="col-12 col-lg-3 col-xl-2 d-none d-lg-block">
+          <img class="logo img-fluid" src="./files/img/icons/logo.svg" alt="maxidoor logo">
+        </div>
+        <div class="col-12 col-md-4 col-xl-3 p-0">
+          <div class="d-inline-block"><img class="img-fluid pr-4 pb-7" src="./files/img/footer/phone-call@2x.png"
+              alt="kontakt logo"></div>
+          <div class="d-inline-block">
+            <p class="font-weight-bold text-uppercase">Kontakt</p>
+            <small>
+              <p>
+                601 530 542<br>
+                601 230 532<br>
+                info@maxidoor.pl
+              </p>
+            </small>
+          </div>
+        </div>
+        <div class="col-12 col-md-5 col-xl-3 p-0">
+          <div class="d-inline-block"><img class="img-fluid pr-4 pb-7" src="./files/img/footer/address@2x.png"
+              alt="adres logo"></div>
+          <div class="d-inline-block">
+            <p class="font-weight-bold text-uppercase">Lokalizacja</p>
+            <small>
+              <p>
+                Galeria Drzwi Maxidoor<br>
+                Konotopa, ul. Piastowska 85<br>
+                05-850 Ożarów Mazowiecki
+              </p>
+            </small>
+          </div>
+        </div>
+        <div class="col-12 col-xl-3 p-0">
+          <div class="d-inline-block"><img class="img-fluid pr-4 pb-7" src="./files/img/footer/clock@2x.png"
+              alt="adres logo"></div>
+          <div class="d-inline-block">
+            <p class="font-weight-bold text-uppercase">Godziny otwarcia</p>
+            <small>
+              <p>
+                Pn-Pt 10:00 - 18:00<br>
+                Sobota 10:00 - 14:00<br>
+                Niedziela nieczynne
+              </p>
+            </small>
+          </div>
+        </div>
+      </div>
+      <div class="footer-bottom row color-white">
+        <div class="col text-left">
+          <p class="d-inline-block"><small>© Maxidoor Sp. z o.o. Wszelkie prawa zastrzeżone</small></p>
+        </div>
+        <div class="col text-right">
+          <p class="d-inline-block"><small>Polityka Prywatności</small></p>
+        </div>
+      </div>
+    </div>
+
+  </footer>
+
+
+  <!-- Bootstrap core JavaScript
+    ================================================== -->
+  <!-- Placed at the end of the document so the pages load faster -->
+  <script src="https://code.jquery.com/jquery-3.5.1.min.js"
+    integrity="sha256-9/aliU8dGd2tb6OSsuzixeV4y/faTqgFtohetphbbj0=" crossorigin="anonymous"></script>
+  <script>window.jQuery || document.write('<script src="./files/external/jquery-3.5.1.min.js"><\/script>')</script>
+  <script src="./files/external/popper.min.js"></script>
+  <script src="./files/external/bootstrap.min.js"></script>
+  <script src="./files/external/holder.min.js"></script>
+  <script src="./files/js/common.js"></script>
+  <script>
+    Holder.addTheme('thumb', {
+      bg: '#55595c',
+      fg: '#eceeef',
+      text: 'Thumbnail'
+    });
+  </script>
+
+
+</body>
+
+</html>
